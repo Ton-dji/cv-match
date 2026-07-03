b3cde998aa3860690b014a3754061dd7c3058c7c
Tondji Capo-chichi <capj01@hotmail.com>
2026-07-03 20:13:16 +0200

    fix: enforce overflow-x-hidden on body to prevent out-of-grid alignment issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden w-full max-w-[100vw]`}
       >
         <Providers>
           <CloudSync />
